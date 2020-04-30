c6c51001320a8a0f4d7c0b405a0f74e03ec8ca74
KamaleshKarthi14 <59138686+KamaleshKarthi14@users.noreply.github.com>
2020-04-30 20:04:37 +0530

    Add files via upload

---
+++ HeartNew_Final.py
@@ -0,0 +1,74 @@
+if __name__ == "__main__":
+
+    #Importing some libraries
+    import numpy as np
+    import pandas as pd
+    
+    #Getting rid of pesky warnings
+    def warn(*args, **kwargs):
+        pass
+    import warnings
+    warnings.warn = warn
+    np.warnings.filterwarnings('ignore')
+
+    column_names = [
+            "age",  #1
+            "sex",  #2
+            "cp",  #3
+            "trestbps",  #4
+            "chol",  #5
+            "fbs",   #6
+            "restecg",  #7
+            "thalach",  #8
+            "exang", #9
+            "oldpeak",  #10
+            "slope",  #11
+            "ca", #12
+            "thal", #13
+            "target"  #14
+        ]
+    #Importing the dataset
+    location = 'Heart.csv'
+    dataset = pd.read_csv(location)
+    dataset = dataset.sample(frac=1).reset_index(drop=True)
+    X = dataset.iloc[:,dataset.columns != 'target'].values
+    y=dataset.iloc[:,dataset.columns=='target'].values
+    
+    #Replace all 'heart-disease' values greater than 0 because my goal is not to classify the disease type
+    #for x,i in enumerate(y):
+     #   if i>0:y[x]=1
+    #Splitting the dataset into the Training set and Test set
+    from sklearn.model_selection._split import train_test_split
+    #from imblearn.combine import SMOTEENN
+    #smote_enn = SMOTEENN()
+    #X_resampled, y_resampled = smote_enn.fit_sample(X, Y)
+    X_train, X_test, Y_Train, Y_Test = train_test_split(X,y, test_size=0.3)
+
+    #Feature scaling
+    #from sklearn.preprocessing import StandardScaler
+    #sc_X = StandardScaler()
+    #X_train = sc_X.fit_transform(X_train)
+    #X_test = sc_X.transform(X_test)
+
+    #Using Pipeline
+    import sklearn.pipeline
+    from sklearn.neural_network import MLPClassifier
+    from sklearn.decomposition import KernelPCA
+    from imblearn.pipeline import make_pipeline
+    
+    #select = sklearn.feature_selection.SelectPercentile(sklearn.feature_selection.f_classif)
+    clf = MLPClassifier(solver='lbfgs', learning_rate='constant', activation='tanh')
+    kernel = KernelPCA()
+    
+    pipeline = make_pipeline(kernel, clf)
+    pipeline.fit(X_train, Y_Train)
+
+    #User-input
+    v = []
+    for i in column_names[:-1]:
+        v.append(input(i+":"))
+    answer = np.array(v)
+    answer = answer.reshape(1,-1)
+    #answer = sc_X.transform(answer)
+    print ("Predicts:"+ str(pipeline.predict(answer)))
+    #print ("("Predicts: " + str(pipeline.predict(answer))")
